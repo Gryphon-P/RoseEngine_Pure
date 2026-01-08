86a03827d2f1954790f648d575921082dab2aaf8
Gryphon Picciottoli <gryphninja@gmail.com>
2026-01-08 18:50:54 -0500

    Create metaprogrammer.py

---
+++ automation_scripts/metaprogrammer.py
@@ -0,0 +1,13 @@
+"""
+The purpose of this file is to create a set of functions that will be able to find and replace certain sections in the game's C++ files.
+"""
+
+def SetEntityTransform(entityName: str, transform: str, sceneName: str):
+    pass # TODO
+    """
+    Find the scene called {sceneName},
+    At the end of the scene's Init() function, write a comment "// Generated from Scene Editor"
+and below that, put all the transforms. something along the lines of f"exampleScene.GetEntityByName(\"{entityName}\").transform = {transform};"
+    """
+    
+    

9ea347b331096a73588b3595859a2fa593f91be7
Gryphon Picciottoli <gryphninja@gmail.com>
2026-01-09 16:45:09 -0500

    Update requirements.py

--- requirements.py
+++ requirements.py
@@ -7,19 +7,16 @@ pipRequirements = (
 )
 
 # Makes the virtual environment
-subprocess.call("python -m venv .venv")
+subprocess.call("python -m venv venv")
 
 # Activates the virtual environment
 if platform == "linux" or platform == "linux2":
-    subprocess.call("source .venv/bin/activate")
-elif platform == "win32":
-    # Tries both commands
-    subprocess.call(".venv/bin/Activate.bat")
-    subprocess.call(".venv/bin/Activate.ps1")
+    subprocess.call("source venv/bin/activate")
+elif platform == "win32":                                                       # Tries both commands
+    subprocess.call("venv/bin/Activate.bat")
+    subprocess.call("venv/bin/Activate.ps1")
 else:
-    # OS is unsupported
-    print(f"ERROR: OS is not supported, manually create virtual environment of name .venv and pip install:\n{str(pipRequirements)}")
+    # OS is unsupported                                                         print(f"ERROR: OS is not supported, manually create virtual environment of name \'venv\' and pip install:\n{str(pipRequirements)}")
 
-# Loops through each requirement
-for lib in pipRequirements:
+# Loops through each requirement                                            for lib in pipRequirements:
     subprocess.call(f"pip install {lib}")

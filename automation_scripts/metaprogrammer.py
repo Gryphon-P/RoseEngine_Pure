1b6c139be635e7fa302e14c7858ac996222241d4
Gryphon Picciottoli <gryphninja@gmail.com>
2026-01-08 19:00:11 -0500

    Update metaprogrammer.py

--- automation_scripts/metaprogrammer.py
+++ automation_scripts/metaprogrammer.py
@@ -8,6 +8,7 @@ def SetEntityTransform(entityName: str, transform: str, sceneName: str):
     Find the scene called {sceneName},
     At the end of the scene's Init() function, write a comment "// Generated from Scene Editor"
 and below that, put all the transforms. something along the lines of f"exampleScene.GetEntityByName(\"{entityName}\").transform = {transform};"
+we will also have to remove all the code below the comment beforehand so that we're not writing it twice.
     """
     
     
